62ec5d3f079c972e6753ac6aa5da2820bc030c01
yash5676 <yashkumargoyal1804@gmail.com>
2024-08-03 02:13:03 +0530

    Frontend done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
-import Homepage from './home/Homepage'
+import React from "react";
+import Home from "./home/Home";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Courses from "./Courses/Courses";
+import Signup from "./components/Signup";
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/AuthProvider";
 
-import {Route,Routes} from "react-router-dom"
-import Courses from './Courses/Courses'
-import Signup from './components/Signup'
 function App() {
-  return(
-  <>
-  <div className="dark:bg-slate-900 dark:text-white">
-  <Routes>
-    <Route path="/" element={<Homepage/>}/>
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
+  return (
+    <>
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/course"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Toaster />
+      </div>
+    </>
+  );
+}
 
-    <Route path="/course" element={<Courses/>}/>
-    <Route path="/signup" element={<Signup/>}/>
-    
-  </Routes>
-  </div>
-  </>
-  )
-  }
-export default App
-
+export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import AuthProvider from "./context/AuthProvider.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <div className="dark:bg-slate-900 dark:text-white">
-    <App />
-    </div>
-    </BrowserRouter>
-  
-  
-)
+    <AuthProvider>
+      <div className="dark:bg-slate-900 dark:text-white">
+        <App />
+      </div>
+    </AuthProvider>
+  </BrowserRouter>
+);
